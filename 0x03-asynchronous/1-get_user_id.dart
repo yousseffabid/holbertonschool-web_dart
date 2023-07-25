a8034e9943fd70d6184f077ad7cfd74d3f0caf09
yousseffabid <abidyoussef58@gmail.com>
2023-07-25 12:37:54 +0100

    user count, get user id

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,15 +1,8 @@
 import 'dart:convert';
+import '1-util.dart';
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
+Future<String> getUserId() async {
+  var iduser = json.decode(await fetchUserData());
 
-getUserId() async {
-  var strRepr = await fetchUserData();
-
-  var Repr  = json.decode(strRepr);
-
-  return Repr["id"];
+  return iduser["id"];
 }
